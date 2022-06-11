b46c6e26df15b76763e7bc8e9ec5c51f85c1ee8a
boluwatifeBE <boluwatifeemmanuel.be@gmail.com>
2022-06-11 15:58:00 +0100

    add-header-logo

--- components/LayoutWrapper.tsx
+++ components/LayoutWrapper.tsx
@@ -1,10 +1,14 @@
 import headerNavLinks from '@/data/headerNavLinks';
+import siteMetadata from '@/data/siteMetadata';
+import Logo from '@/data/logo.svg';
+import Logo2 from '@/data/logo2.svg';
 import { ReactNode } from 'react';
 import Footer from './Footer';
 import Link from './Link';
 import MobileNav from './MobileNav';
 import SectionContainer from './SectionContainer';
 import ThemeSwitch from './ThemeSwitch';
+import Image from 'next/image';
 
 interface Props {
   children: ReactNode;
@@ -14,7 +18,29 @@ const LayoutWrapper = ({ children }: Props) => {
   return (
     <SectionContainer>
       <div className='flex h-screen flex-col justify-between'>
-        <header className='flex items-center justify-end py-8'>
+        <header className='flex items-center justify-between py-10'>
+          <div>
+            <Link href='/' aria-label={siteMetadata.headerTitle}>
+              <div className='flex items-center justify-between'>
+                <div className='mr-3'>
+                  <Image
+                    src={'/static/images/logo.png'}
+                    width={38}
+                    height={38}
+                    layout='fixed'
+                    // objectFit='contain'
+                  />
+                </div>
+                {typeof siteMetadata.headerTitle === 'string' ? (
+                  <div className='hidden h-6 text-xl font-semibold sm:block'>
+                    {siteMetadata.headerTitle}
+                  </div>
+                ) : (
+                  siteMetadata.headerTitle
+                )}
+              </div>
+            </Link>
+          </div>
           <div className='flex items-center text-base leading-5'>
             <div className='hidden sm:block'>
               {headerNavLinks.map(link => (
