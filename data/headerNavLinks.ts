6c89c357d4e942ce9528a8c153378fcd317db132
Ifeoluwa <iadewunm@mercuryholidays.com>
2022-06-12 20:15:03 +0100

    add careers page and header

--- data/headerNavLinks.ts
+++ data/headerNavLinks.ts
@@ -1,10 +1,11 @@
 const headerNavLinks = [
   { href: '/', title: 'Home' },
+  { href: '/careers', title: 'Careers' },
+  { href: '/about', title: 'About' },
   { href: '/blog', title: 'Blog' },
   { href: '/courses', title: 'Courses' },
-  { href: '/projects', title: 'Projects' },
-  { href: '/about', title: 'About' },
-  { href: '/contact', title: 'Contact' },
+  // { href: '/projects', title: 'Projects' },
+  // { href: '/contact', title: 'Contact' },
 ];
 
 export default headerNavLinks;
