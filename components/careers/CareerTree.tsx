6fb27e382a8d137b10fff4038a86c0562bd833de
Ifeoluwa <iadewunm@mercuryholidays.com>
2022-06-16 22:01:50 +0100

    Update career tree

--- components/careers/CareerTree.tsx
+++ components/careers/CareerTree.tsx
@@ -1,24 +1,24 @@
 import React from 'react'
 
 const Parent = (item) => (
-    <a href={`${item.path}`}>
-        <h1 className="parent text-xl font-extrabold leading-9 tracking-tight text-gray-800 dark:text-gray-100 sm:text-2xl sm:leading-10 md:text-3xl md:leading-14">
+    <a href={item.path}>
+        <h1 className={`${item.level} text-xl font-extrabold leading-9 tracking-tight text-gray-800 dark:text-gray-100 sm:text-2xl sm:leading-10 md:text-3xl md:leading-14`}>
             {item.name}
         </h1>
     </a>
 );
 
 const Child = (item) => (
-    <a href={`${item.path}`}>
-        <p className='child'>
+    <>
+        <p key={item.name} className={item.level}>
             {item.name}
         </p>
-        {item.children.map((ch) => (
-            <span className='grand-child'>
-                <Child {...ch} />
-            </span>
+        {item.children.map((child) => (
+            <div key={child.name} className={child.level}>
+                <Child {...child} />
+            </div>
         ))}
-    </a>
+    </>
 );
 
 export default function Tree(props) {
@@ -26,11 +26,11 @@ export default function Tree(props) {
         <>
             {
                 props.data.map((item) => (
-                    <div>
+                    <div key={item.name}>
                         <Parent {...item} />
-                        <ul>
-                            {item.children.map((ch) => (
-                                <li><Child {...ch} /></li>
+                        <ul key={item.name}>
+                            {item.children.map((child) => (
+                                <li key={child.name}><Child {...child} /></li>
                             ))}
                         </ul>
                     </div>
