3d20bd5b9cac31c52062fa9ad9698020ea4d7390
Ifeoluwa <iadewunm@mercuryholidays.com>
2022-09-22 17:05:49 +0100

    [feat]:update article tags and header

--- data/headerNavLinks.ts
+++ data/headerNavLinks.ts
@@ -1,8 +1,8 @@
 const headerNavLinks = [
   { href: '/', title: 'Home' },
   { href: '/careers', title: 'Paths' },
-  { href: '/topics/advice', title: 'Advices' },
-  { href: '/topics/story', title: 'Stories' },
+  { href: '/topics/advice', title: 'Advice' },
+  { href: '/topics/story', title: 'Story' },
   // { href: '/courses', title: 'Guides' },
   // { href: '/projects', title: 'Tools' },
   // { href: '/contact', title: 'Contact' },
